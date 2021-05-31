dba44da339e19303408f7de84f6858a5bb36b25a
Nisarg <72545171+itsNisarg@users.noreply.github.com>
2021-05-31 20:06:04 +0530

    Lecture2

---
+++ Output.java
@@ -0,0 +1,11 @@
+
+public class Output {
+    public static void main(String[] args)
+    {
+        int a=8;
+        float b=6.7f;
+        System.out.printf("\n a = %d and b = %f",a,b);
+        System.out.format("\n a = %d and b = %f",a,b);
+        System.out.printf("\n a = %4d and b = %8.2f",a,b);
+    }
+}
