5e38acdb6695d2a1f0275f903159189ce922ca91
Nisarg <72545171+itsNisarg@users.noreply.github.com>
2021-06-04 20:20:04 +0530

    Lecture
    Introducing the class concept

---
+++ Classes.java
@@ -0,0 +1,28 @@
+class Box
+{
+    private int length,width,height;
+    public
+    void setDimension(int l,int w,int h)
+    {
+        length=l;
+        width=w;
+        height=h;
+    }
+    void showDimension()
+    {
+        System.out.println("length : "+length);
+        System.out.println("height : "+height);
+        System.out.println("width : "+width);
+    }
+}
+public class Classes {
+    public static void main(String[] args)
+    {
+        Box smallBox=new Box();
+        smallBox.setDimension(1,2,3);
+        smallBox.showDimension();
+        smallBox=new Box();
+        smallBox.showDimension();
+
+    }
+}
---
+++ Spaces.java
@@ -0,0 +1,8 @@
+public class Spaces {
+    public static void main(String [] args)
+    {
+        String str="Nisarg Suthar";
+        System.out.println("String with spacing : "+str);
+        System.out.println("String after replacing space with _ : "+str.replace(' ','_'));
+    }
+}
