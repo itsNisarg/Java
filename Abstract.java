3271287abf8ce1b8c9fd08eed41d8306f7449117
Nisarg <72545171+itsNisarg@users.noreply.github.com>
2021-06-30 19:17:50 +0530

    Add files via upload

---
+++ Abstract.java
@@ -0,0 +1,21 @@
+abstract class Acc1
+{
+    private  int age;
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
+
+public class Abstract {
+    public static void main(String [] args)
+    {
+        //Acc a = new Acc1() ; the class Acc is abstract and hence cannot be instantiated
+    }
+}
